760cf36df0b87c41617d000ff822c464f66cb097
RUDHRA PHARMACEUTICALS <trivenireddy386@gmail.com>
2026-09-25 12:28:55 +0530

    Add simple product catalogue data file

---
+++ products-data.js
@@ -0,0 +1,14 @@
+// RUDHRA PHARMACEUTICALS product catalogue
+// Add new products by copying one product object and changing the details.
+// Example:
+// {
+//   image: "images/product-name.jpg",
+//   name: "Product Name",
+//   manufacturer: "Manufacturer Name",
+//   composition: "Composition / Strength",
+//   pack: "Pack Size",
+//   category: "Tablets & Capsules",
+//   description: "Short approved product description"
+// }
+
+const products = [];
